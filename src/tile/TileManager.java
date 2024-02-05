128e4917cd361a2b8c024243892caebd2a6a83a6
Panoppar <157826708+Panoppar@users.noreply.github.com>
2024-02-05 23:09:40 +0700

    Map Rendering

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,39 +1,111 @@
 package tile;
 
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
+
 import main.GamePanel;
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum[][];
 
     public TileManager(GamePanel gp) {
 
         this.gp = gp;
 
         tile = new Tile[10]; // create 10 kind of tile, Could be change later.
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
+        loadMap("res/maps/mapTest.txt"); // change file Path here.
     }
 
     // Create a new kind of tile here (inside try...catch) by 
     //tile[1] = new Tile();
-    //tile[1].image = imageIO.read(getClass().getResourceAsStream("image directory")) 
+    //tile[1].image = ImageIO.read(new FileInputStream("Image directory")); 
     public void getTileImage() {
 
-        try{
+        try {
 
-        tile[0] = new Tile();
-        tile[0].image = imageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(new FileInputStream("res/tiles/TilesetG5.png"));
 
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(new FileInputStream("res/tiles/TilesetG60.png"));
         } catch(IOException e) {
             e.printStackTrace();
         }
     }
 
+    // scan text file line by line and split them 
+    // into single number which we will use later.
+    // the param filePath allows us to upload more map into the game in the future. 
+    public void loadMap(String filePath) {
+        try {
+
+            // Import text file(map)
+            InputStream is = new FileInputStream(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+                // Read text file line by line
+                String line = br.readLine();
+
+                while(col < gp.maxScreenCol) {
+
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if(col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+
+        } catch(Exception e) {
+
+        }
+    }
+
     public void draw(Graphics2D g2) {
 
-        g2.drawImage(tile[0].image, 0, 0, gp.titleSize, gp.tileSize, null);
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if(col == gp.maxScreenCol) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
+            }
+        }
     }
 
 }
