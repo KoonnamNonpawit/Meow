4a6943671046f6516d542214715f1af9d2376a81
Panoppar <157826708+Panoppar@users.noreply.github.com>
2024-02-07 14:05:34 +0700

    PAUSE State added

--- src/main/UI.java
+++ src/main/UI.java
@@ -2,14 +2,17 @@ package main;
 
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.text.DecimalFormat;
+
+import object.OBJ_Paper;
 
 public class UI {
     
-
     GamePanel gp;
     Graphics2D g2;
     Font arial_40;
+    BufferedImage paperImage;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -22,7 +25,8 @@ public class UI {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
-        
+        OBJ_Paper paper = new OBJ_Paper();
+        paperImage = paper.image;
     }
 
     public void showMessage(String text) {
@@ -30,7 +34,33 @@ public class UI {
         message = text;
         messageOn = true;
     }
+
     public void draw(Graphics2D g2) {
 
+        this.g2 = g2;
+
+        if(gp.gameState == gp.playState) {
+            // Do playState Stuff later
+        }
+        if(gp.gameState == gp.pauseState) {
+            drawPauseScreen();
+        }
+    }
+    
+    public void drawPauseScreen() {
+
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));
+        String text = "PAUSED";
+        int x = getXforCenteredText(text);
+        int y = gp.screenHeight/2;
+
+        g2.drawString(text, x, y);
+    }
+
+    public int getXforCenteredText(String text) {
+
+        int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        int x = gp.screenWidth/2 - length/2;
+        return x;
     }
 }
