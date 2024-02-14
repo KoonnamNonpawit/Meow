e7d2e320f77d45b62006670af031b773e4d4d62e
Panoppar <157826708+Panoppar@users.noreply.github.com>
2024-02-14 17:10:22 +0700

    Add SaveLoad class
    the actual system will be add later after puzzle progress, item inside chest, inventory was fully done.

---
+++ src/data/DataStorage.java
@@ -0,0 +1,14 @@
+package data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class DataStorage implements Serializable{
+    
+    // PUZZLE PROGRESSION
+
+
+    // PLAYER'S INVENTORY
+    ArrayList<String> itemsName = new ArrayList<>();
+    //ArrayList<Integer> itemAmounts = new ArrayList<>();
+}
