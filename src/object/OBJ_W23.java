a0dfd5c2c5c267846c377fc2d6c56581e41bd861
KoonnamNonpawit <koonnam148@gmail.com>
2024-02-16 01:02:58 +0700

    Some collision

--- src/object/OBJ_W23.java
+++ src/object/OBJ_W23.java
@@ -11,5 +11,12 @@ public class OBJ_W23 extends Entity{
         name = "W23";
         down1 = setup("res/tiles/TilesetW23");
      
+        solidArea.x = 0;
+        solidArea.y = 0;
+        solidArea.width = 32*3;
+        solidArea.height = 32*3;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        collision = true;
     }
 }
--- src/object/OBJ_W25.java
+++ src/object/OBJ_W25.java
@@ -12,5 +12,12 @@ public class OBJ_W25 extends Entity {
         name = "W25";
         down1 = setup("res/tiles/TilesetW25");
       
+        solidArea.x = 0;
+        solidArea.y = 0;
+        solidArea.width = 32*3;
+        solidArea.height = 32*3;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        collision = true;
     }
 }
